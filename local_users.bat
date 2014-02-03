035fc188aaf80080f91fbf684e29b38710107431
Radnus <Rodney.Visser@gmail.com>
2014-02-03 17:54:29 -0600

    1'ish'-liner update for localusers
    Need to also do /domain!

--- local_users.bat
+++ local_users.bat
@@ -8,6 +8,7 @@ type %computername%-localusers.tmp | findstr /v The | findstr /v command | finds
 
 del %computername%-localusers.tmp
 
-:: Win 2-liner below
-:: FOR /F "tokens=1,2,3 skip=4" %I in ('net user') do echo %I >> %computername%-localusers.tmp & echo %J >> %computername%-localusers.tmp & echo %K >> %computername%-localusers.tmp
-:: type %computername%-localusers.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO && del %computername%-localusers.tmp
+:: Win 1'ish'-liner below
+:: FOR /F "tokens=1,2,3 skip=4" %I in ('net user') do echo %I >> %computername%-localusers.tmp & echo %J >> %computername%-localusers.tmp & echo %K >> %computername%-localusers.tmp && type %computername%-localusers.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO >> %computername%-localusers.txt
+:: type %computername%-localusers.txt
+:: del %computername%-localusers.tmp
