8448afe00d2ee07d015203a79a0753ef175be298
Radnus <Rodney.Visser@gmail.com>
2014-02-04 09:42:25 -0600

    Update domain_users.bat

--- domain_users.bat
+++ domain_users.bat
@@ -3,11 +3,19 @@
 :: Domain Checks
 
 echo Collecting Domain Users...
-FOR /F "tokens=1,2,3 skip=4" %%I in ('net user /domain') do echo %%I >> %userdomain%-users.tmp & echo %%J >> %userdomain%-users.tmp & echo %%K >> %userdomain%-users.tmp
-type %userdomain%-users.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO > %userdomain%-users.txt
+FOR /F "tokens=1,2,3 skip=6" %%I in ('net user /domain') do echo %%I >> %userdomain%-domain-users.tmp & echo %%J >> %userdomain%-domain-users.tmp & echo %%K >> %userdomain%-domain-users.tmp
+type %userdomain%-domain-users.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO > %userdomain%-domain-users.txt
 
-del %userdomain%-users.tmp
+del %userdomain%-domain-users.tmp
 
 :: Win 2-liner below
-:: FOR /F "tokens=1,2,3 skip=4" %I in ('net user /domain') do echo %I >> %userdomain%-users.tmp & echo %J >> %userdomain%-users.tmp & echo %K >> %userdomain%-users.tmp
-:: type %userdomain%-users.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO && del %computername%-localusers.tmp
+:: FOR /F "tokens=1,2,3 skip=6" %I in ('net user /domain') do echo %I >> %userdomain%-domain-users.tmp & echo %J >> %userdomain%-domain-users.tmp & echo %K >> %userdomain%-domain-users.tmp
+:: type %userdomain%-domain-users.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO >> %userdomain%-domain-users.txt
+:: type %userdomain%-domain-users.txt
+:: del %computername%-domain-users.tmp
+
+echo Collecting Domain Users Details...
+FOR /F "tokens=1" %%I in (%userdomain%-domain-users.txt) do net user /domain %%I >> %userdomain%-domain-users-details.txt
+
+:: Win 1-liner below
+:: FOR /F "tokens=1" %I in (%userdomain%-domain-users.txt) do net user /domain %I >> %userdomain%-domain-users-details.txt
