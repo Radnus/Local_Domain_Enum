c88ecb33b62799b5df3aa7fe5c6c8940962dbb4c
Radnus <Rodney.Visser@gmail.com>
2014-02-04 09:42:59 -0600

    Update local_groups.bat

--- local_groups.bat
+++ local_groups.bat
@@ -7,4 +7,4 @@ del %computername%-localgroup-members.txt
 for /F "skip=4 delims=*" %%i in ('net localgroup') do @net localgroup "%%i" >> %computername%-localgroup-members.txt
 
 :: Win 1-liner below
-:: for /F "skip=4 delims=*" %i in ('net localgroup') do @net localgroup "%i" 
+:: for /F "skip=4 delims=*" %i in ('net localgroup') do @net localgroup "%i" >> %computername%-localgroup-members.txt
