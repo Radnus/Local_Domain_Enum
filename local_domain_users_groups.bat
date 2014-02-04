8f1ff192c190ff38dd7c185f79f66433eddebe1f
Radnus <Rodney.Visser@gmail.com>
2014-02-04 08:14:04 -0600

    1-ish liner = !work
    1-ish liner didn't work = reverted & added user details

--- local_domain_users_groups.bat
+++ local_domain_users_groups.bat
@@ -1,4 +1,4 @@
-@echo off 
+@echo off
 
 :: ~~~Local Checks~~~
 
@@ -8,10 +8,17 @@ type %computername%-localusers.tmp | findstr /v The | findstr /v command | finds
 
 del %computername%-localusers.tmp
 
-:: Win 1'ish'-liner below
-:: FOR /F "tokens=1,2,3 skip=4" %I in ('net user') do echo %I >> %computername%-localusers.tmp & echo %J >> %computername%-localusers.tmp & echo %K >> %computername%-localusers.tmp && type %computername%-localusers.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO >> %computername%-localusers.txt
+:: Win !1'ish'-liner below
+:: FOR /F "tokens=1,2,3 skip=4" %I in ('net user') do echo %I >> %computername%-localusers.tmp & echo %J >> %computername%-localusers.tmp & echo %K >> %computername%-localusers.tmp 
+:: type %computername%-localusers.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO >> %computername%-localusers.txt
 :: type %computername%-localusers.txt
 :: del %computername%-localusers.tmp
+
+echo Collecting Local Users Details...
+FOR /F "tokens=1" %%I in (%computername%-localusers.txt) do net user %%I >> %computername%-localusers-details.txt
+
+:: Win 1-liner below
+:: FOR /F "tokens=1" %I in (%computername%-localusers.txt) do net user %I >> %computername%-localusers-details.txt
 
 echo Collecting Localgroup Members...
 del %computername%-localgroup-members.txt
