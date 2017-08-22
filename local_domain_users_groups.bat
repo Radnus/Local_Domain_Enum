9e22f1e56c37d03209462937df6fa2bf50df374c
Radnus <Rodney.Visser@gmail.com>
2017-08-22 12:56:04 -0500

    Update local_domain_users_groups.bat

--- local_domain_users_groups.bat
+++ local_domain_users_groups.bat
@@ -39,7 +39,7 @@ del %userdomain%-domain-users.tmp
 :: FOR /F "tokens=1,2,3 skip=6" %I in ('net user /domain') do echo %I >> %userdomain%-domain-users.tmp & echo %J >> %userdomain%-domain-users.tmp & echo %K >> %userdomain%-domain-users.tmp
 :: type %userdomain%-domain-users.tmp | findstr /v The | findstr /v command | findstr /v completed | findstr /v ECHO >> %userdomain%-domain-users.txt
 :: type %userdomain%-domain-users.txt
-:: del %computername%-domain-users.tmp
+:: del %userdomain%-domain-users.tmp
 
 echo Collecting Domain Users Details...
 FOR /F "tokens=1" %%I in (%userdomain%-domain-users.txt) do net user /domain %%I >> %userdomain%-domain-users-details.txt
